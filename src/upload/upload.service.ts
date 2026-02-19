649d43e64ed745f93c034ceb6f99a09146481e21
Nadlar Aditya <nadlaraditya13@gmail.com>
2026-02-19 14:00:47 +0700

    update: schema db upload

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UploadService {}
+export class UploadService {
+  getFileUrl(filename: string): string {
+    return `${process.env.BACKEND_URL || 'http://localhost:4000'}/uploads/${filename}`;
+  }
+}
